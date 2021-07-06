dd68e4d23ca1d969d14c90aca9e2558ee338efba
davidgb9716 <83733181+davidgb9716@users.noreply.github.com>
2021-07-06 00:15:55 -0500

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,24 @@
+import random
+
+characterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '#', '$', '&', '%', '.']
+
+while True:
+	try:
+		password_length = int(input('How many characters does your password need to include? '))
+	except ValueError:
+		print('Please input an integer.')
+		continue
+	if password_length <= 0:
+		print('Please input a positive integer above zero.')
+	else:
+		break
+
+password_list = []
+
+for char in range(0, password_length):
+	rand_num = random.randint(0, len(characterList) - 1)
+	password_list.append(characterList[rand_num])
+
+password = ''.join(password_list)
+
+print('Your password is:', password) 
